272bd4709d5ec41231c09e4165329c6ea36e612a
Izabela <i.kazimava@gmail.com>
2021-06-11 20:16:53 +0200

    Исправление ошибок линтера - 2

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 function getRandomInteger(min, max) {
-  if (typeof min !== 'number' || typeof max !== 'number') {return null};
-  if (min < 0 || max <= min) {return null};
+  if (typeof min !== 'number' || typeof max !== 'number') {return null;}
+  if (min < 0 || max <= min) {return null;}
   return Math.floor(min + Math.random() * (max - min +1));
 }
 getRandomInteger(1, 3);
 
 function randomFloatNumber(min, max, decimalPlace) {
-  if (typeof min !== 'number' || typeof max !== 'number' || typeof decimalPlace !== 'number') {return null};
-  if (min < 0 || max <= min) {return null};
+  if (typeof min !== 'number' || typeof max !== 'number' || typeof decimalPlace !== 'number') {return null;}
+  if (min < 0 || max <= min) {return null;}
   return (min + Math.random() * (max - min +1)).toFixed(decimalPlace);
 }
 randomFloatNumber(2.2, 5.3, 3);
